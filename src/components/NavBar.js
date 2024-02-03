eb5df1bee8c1e23780f6337e617d948f40af04ca
Gabriel <gabriel.fortia.pro@gmail.com>
2024-02-03 18:30:29 +0100

    feat:improve navbar design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,11 @@ const CustomLink = ({ href, title, className = "" }) => {
   return (
     <Link
       href={href}
-      className={`${className} relative group text-gray-600 font-medium`}
+      className={`${className} relative group text-gray-600 font-semibold`}
     >
       {title}
       <span
-        className={`h-[1px]  inline-block bg-amber-600 absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
+        className={`h-[1px]  inline-block bg-orange-500 absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
           dark:bg-light ${router.asPath === href ? "w-full" : "w-0"}`}
       >
         &nbsp;
@@ -23,20 +23,22 @@ const CustomLink = ({ href, title, className = "" }) => {
 
 const NavBar = () => {
   return (
-    <header className="w-full px-36 py-16 flex items-center justify-between relative z-10">
+    <header className="w-full px-60 py-16 flex items-center justify-between relative z-10">
       <div className="w-full flex justify-between items-center">
-        <h1 className="font-semibold text-2xl text-amber-600  ">
+        <h1 className="font-semibold text-2xl text-orange-400  ">
           Gabriel Fortià
         </h1>
-        <nav className="flex gap-5">
+        <nav className="flex gap-16">
           <CustomLink href="/" title="Home" />
           <CustomLink href="/about" title="About" />
           <CustomLink href="/articles" title="Articles" />
           <CustomLink href="/subscribe" title="Subscribe" />
+        </nav>
+        <div className="flex gap-6">
           <LinkedInIcon />
           <TwitterIcon />
           <MoonIcon />
-        </nav>
+        </div>
       </div>
     </header>
   );
