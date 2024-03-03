729220d774ec3721999e4cc1c1e2a7ff74043862
gabcat <gabriel.fortia.pro@gmail.com>
2024-03-03 17:10:58 +0100

    feat: add responsiveness to the footer's component

--- src/components/CustomLink.js
+++ src/components/CustomLink.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion"
 import Link from "next/link";
 import { buttonVariants } from "../animationVariants.js";
 
-const CustomLink = ({ title, route, className = "", icon: Icon, target }) => {
+const CustomLink = ({ title, route, className = "", iconClassname = "", icon: Icon, target }) => {
     return (
         <motion.div
             initial="initial"
@@ -16,7 +16,7 @@ const CustomLink = ({ title, route, className = "", icon: Icon, target }) => {
                 href={route}
                 className={`p-2.5 px-6 flex rounded-lg text-lg font-semibold hover:shadow-lg justify-center ${className}`}
             >
-                {Icon && <Icon className="mr-2 !w-6 h-auto" />}
+                {Icon && <Icon className={`mr-2 !w-6 h-auto ${iconClassname}`} />}
                 {title}
             </Link>
         </motion.div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,33 +11,35 @@ const Footer = () => {
     <div>
       <Layout className="!px-0 !pb-8 !min">
         {/* <footer className="border-t-2 border-solid border-gray-200 mt-20 flex justify-center"> */}
-        <footer className="bg-gray-200 mx-8 px-4 lg:mx-64 rounded-full p-1 shadow-xl shadow-orange-100 pb-5">
-          <div className="flex justify-center items-center">
-            <MyBlogLogo className="mt-6 lg:h-60 h-40 shrink flex " />
-            <div className="mt-10 flex-col flex gap-5 items-center ">
-              <h2 className="font-medium text-2xl">Thanks for visitting!</h2>
-              <h3 className="text-lg text-center w-96">Your presence is appreciated. Feel free to reach out anytime, I'd love to connect!</h3>
+        <footer className="bg-gray-200 mx-8 px-4 md:mx-36  xl:mx-64 md:rounded-full rounded-xl p-1 shadow-xl shadow-orange-100 pb-5">
+          <div className="flex justify-center items-center flex-col md:flex-row">
+            <MyBlogLogo className="mt-6 lg:h-52 h-40 shrink flex order-last md:order-first " />
+            <div className="mt-10 flex-col flex lg:gap-5 gap-3 items-center ">
+              <h2 className="font-semibold text-lg md:text-xl lg:text-2xl">Thanks for visitting!</h2>
+              <h3 className="lg:text-lg text-sm text-center w-56 md:w-64 lg:w-96">Your presence is appreciated. Feel free to reach out anytime, I'd love to connect!</h3>
               <div className="flex md:flex-row flex-col gap-4">
                 <CustomLink
                   route={myLinkedinUrl}
-                  className="border-solid border-2  border-secondary text-secondary"
+                  className="border-solid border-2  border-secondary text-secondary lg:!px-6 lg:!text-lg !px-2 !text-sm"
+                  iconClassname="!w-5"
                   title="Connect"
                   icon={LinkedinOutlineIcon}
                 />
                 <CustomLink
                   route={myMailUrl}
-                  className="border-solid border-2 border-secondary text-secondary"
+                  className="border-solid border-2  border-secondary text-secondary lg:!px-6 lg:!text-lg !px-2 !text-sm"
+                  iconClassname="!w-5"
                   title="Email"
                   icon={EnvelopeIcon}
                 />
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-center mt-6 gap-3">
-            <div className=" text-sm text-gray-800">
+          <div className="flex flex-col md:flex-row items-center justify-center mt-6 gap-3 md:mb-0 mb-5">
+            <div className=" lg:text-sm text-xs text-gray-800">
               &copy; {new Date().getFullYear()}. All Rights Reserved.
             </div>
-            <div className="text-sm text-gray-800 text-center">Built with &#10084; by Gabriel Fortià</div>
+            <div className="text-xs lg:text-sm text-gray-800 text-center">Built with &#10084; by Gabriel Fortià</div>
           </div>
         </footer>
       </Layout>
